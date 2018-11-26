000cfac8f68312f63915aaa1162a1bbbe980cf67
josevsaraiva@gmail.com <josevsaraiva@gmail.com>
2018-11-26 15:54:49 -0300

    x

--- mvc-php-basico-1/App/Views/home/index.php
+++
@@ -1,5 +0,0 @@
-<div class="container">
-     <div class="starter-template">
-            <h1>Primeira aplicação MVC em PHP</h1>
-    </div>
-</div>
--- mvc-php-basico-1/App/Views/usuario/sucesso.php
+++
@@ -1,9 +0,0 @@
-<div class="container">
-    <div class="row">
-        <br>
-        <div class="col-md-12">
-            <h3>Parabéns <?php echo $Sessao::retornaValorFormulario('nome');?>, seu cadastro foi realizado com sucesso.</h3>
-            <a href="http://<?php echo APP_HOST; ?>/usuario/cadastro" class="btn btn-info">Voltar</a>
-        </div>
-    </div>
-</div>
